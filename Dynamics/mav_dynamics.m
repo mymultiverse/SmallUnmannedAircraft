870e27d33f3042327a44d59b5c39d4e61283a063
Nebula <yrajsm@gmail.com>
2018-07-17 18:57:20 +0900

    Create mav_dynamics.m

---
+++ Dynamics/mav_dynamics.m
@@ -0,0 +1,219 @@
+function [sys,x0,str,ts,simStateCompliance] = mav_dynamics(t,x,u,flag,P)
+
+switch flag,
+
+  %%%%%%%%%%%%%%%%%%
+  % Initialization %
+  %%%%%%%%%%%%%%%%%%
+  case 0,
+    [sys,x0,str,ts,simStateCompliance]=mdlInitializeSizes(P);
+
+  %%%%%%%%%%%%%%%
+  % Derivatives %
+  %%%%%%%%%%%%%%%
+  case 1,
+    sys=mdlDerivatives(t,x,u,P);
+
+  %%%%%%%%%%
+  % Update %
+  %%%%%%%%%%
+  case 2,
+    sys=mdlUpdate(t,x,u);
+
+  %%%%%%%%%%%
+  % Outputs %
+  %%%%%%%%%%%
+  case 3,
+    sys=mdlOutputs(t,x,u);
+
+  %%%%%%%%%%%%%%%%%%%%%%%
+  % GetTimeOfNextVarHit %
+  %%%%%%%%%%%%%%%%%%%%%%%
+  case 4,
+    sys=mdlGetTimeOfNextVarHit(t,x,u);
+
+  %%%%%%%%%%%%%
+  % Terminate %
+  %%%%%%%%%%%%%
+  case 9,
+    sys=mdlTerminate(t,x,u);
+
+  %%%%%%%%%%%%%%%%%%%%
+  % Unexpected flags %
+  %%%%%%%%%%%%%%%%%%%%
+  otherwise
+    DAStudio.error('Simulink:blocks:unhandledFlag', num2str(flag));
+end
+
+% end sfuntmpl
+
+%
+%=============================================================================
+% mdlInitializeSizes
+% Return the sizes, initial conditions, and sample times for the S-function.
+%=============================================================================
+%
+function [sys,x0,str,ts,simStateCompliance]=mdlInitializeSizes(P)
+
+%
+% call simsizes for a sizes structure, fill it in and convert it to a
+% sizes array.
+%
+% Note that in this example, the values are hard coded.  This is not a
+% recommended practice as the characteristics of the block are typically
+% defined by the S-function parameters.
+%
+sizes = simsizes;
+
+sizes.NumContStates  = 12;
+sizes.NumDiscStates  = 0;
+sizes.NumOutputs     = 12;
+sizes.NumInputs      = 6;
+sizes.DirFeedthrough = 0;
+sizes.NumSampleTimes = 1;   % at least one sample time is needed
+
+sys = simsizes(sizes);
+
+%
+% initialize the initial conditions
+%
+x0  = [...
+    P.pn0;...
+    P.pe0;...
+    P.pd0;...
+    P.u0;...
+    P.v0;...
+    P.w0;...
+    P.phi0;...
+    P.theta0;...
+    P.psi0;...
+    P.p0;...
+    P.q0;...
+    P.r0;...
+    ];
+
+%
+% str is always an empty matrix
+%
+str = [];
+
+%
+% initialize the array of sample times
+%
+ts  = [0 0];
+
+% Specify the block simStateCompliance. The allowed values are:
+%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
+%    'DefaultSimState', < Same sim state as a built-in block
+%    'HasNoSimState',   < No sim state
+%    'DisallowSimState' < Error out when saving or restoring the model sim state
+simStateCompliance = 'UnknownSimState';
+
+
+% end mdlInitializeSizes
+
+%
+%=============================================================================
+% mdlDerivatives
+% Return the derivatives for the continuous states.
+%=============================================================================
+%
+function sys=mdlDerivatives(t,x,uu,P)
+
+    pn    = x(1);
+    pe    = x(2);
+    pd    = x(3);
+    u     = x(4);
+    v     = x(5);
+    w     = x(6);
+    phi   = x(7);
+    theta = x(8);
+    psi   = x(9);
+    p     = x(10);
+    q     = x(11);
+    r     = x(12);
+    fx    = uu(1);
+    fy    = uu(2);
+    fz    = uu(3);
+    ell   = uu(4);
+    m     = uu(5);
+    n     = uu(6);
+    mass = P.mass;
+    tau=P.Jx*P.Jz-P.Jxz*P.Jxz;
+    tau1=P.Jxz*(P.Jx -P.Jy+P.Jz)/tau;
+    tau2=(P.Jz*(P.Jz -P.Jy)+P.Jxz*P.Jxz)/tau;
+    tau3=P.Jz/tau;
+    tau4=P.Jxz/tau;
+    tau5=(P.Jz -P.Jx)/P.Jy;
+    tau6=P.Jxz/P.Jy;
+    tau7=(P.Jx*(P.Jx -P.Jy)+P.Jxz*P.Jxz)/tau;
+    tau8=P.Jx/tau;
+  
+    pndot = cos(theta)*cos(psi)*u+(sin(phi)*sin(theta)*cos(psi)-cos(theta)*sin(phi))*v+(cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi))*w;
+    pedot = cos(theta)*cos(psi)*u+(sin(phi)*sin(theta)*cos(psi)+cos(theta)*sin(phi))*v+(cos(phi)*sin(theta)*cos(psi)-sin(phi)*sin(psi))*w;
+    pddot = -sin(theta)*u+sin(phi)*cos(theta)*v+cos(phi)*cos(theta)*w;
+    udot = r*v-q*w+fx/mass;
+    vdot = p*w-r*u+fy/mass;
+    wdot = q*u-p*v+fz/mass;
+    phidot = p+sin(phi)*tan(theta)*q+cos(phi)*tan(theta)*r;
+    thetadot = cos(phi)*q-sin(phi)*r;
+    psidot = (sin(phi)/cos(theta))*q+(cos(phi)/cos(theta))*r;
+    pdot = tau1*p*q-tau2*q*r+tau3*ell+tau4*n;
+    qdot = tau5*p*r-tau6*(p*p-r*r)+m/P.Jy;
+    rdot = tau7*p*q-tau1*q*r+tau4*ell+tau8*n;
+
+sys = [pndot; pedot; pddot; udot; vdot; wdot; phidot; thetadot; psidot; pdot; qdot; rdot];
+% end mdlDerivatives
+
+%
+%=============================================================================
+% mdlUpdate
+% Handle discrete state updates, sample time hits, and major time step
+% requirements.
+%=============================================================================
+%
+function sys=mdlUpdate(t,x,u)
+
+sys = [];
+
+
+% end mdlUpdate
+
+%
+%=============================================================================
+% mdlOutputs
+% Return the block outputs.
+%=============================================================================
+%
+function sys=mdlOutputs(t,x,u)
+
+sys = x;
+% end mdlOutputs
+
+%
+%=============================================================================
+% mdlGetTimeOfNextVarHit
+% Return the time of the next hit for this block.  Note that the result is
+% absolute time.  Note that this function is only used when you specify a
+% variable discrete-time sample time [-2 0] in the sample time array in
+% mdlInitializeSizes.
+%=============================================================================
+%
+function sys=mdlGetTimeOfNextVarHit(t,x,u)
+
+sampleTime = 1;    %  Example, set the next hit to be one second later.
+sys = t + sampleTime;
+
+% end mdlGetTimeOfNextVarHit
+
+%
+%=============================================================================
+% mdlTerminate
+% Perform any end of simulation tasks.
+%=============================================================================
+%
+function sys=mdlTerminate(t,x,u)
+
+sys = [];
+
+% end mdlTerminate
